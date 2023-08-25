6c9be11033f2e3522f485cad35326f04dc18d2b7
Scott Speers <128268586+ScottyCodeman@users.noreply.github.com>
2023-08-24 21:00:22 -0400

    got first page done
    got first page for stock input and manual selection created

--- pages/stock_input.py
+++ pages/stock_input.py
@@ -1,5 +1,6 @@
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 from streamlit_extras.chart_container import chart_container
+import extra_streamlit_components as stx
 from markdownlit import mdlit
 import pandas as pd
 import datetime
@@ -17,13 +18,40 @@ st.set_page_config(
 st.markdown("# Stock sector selection")
 st.sidebar.header("Stock sector selection")
 
-#@st.experimental_memo
-
+#setting tabs using extras
+tab = stx.tab_bar(data=[
+    stx.TabBarItemData(id="Sector Select", title="📈 Sector Select", description="Choose from three sectors"),
+    stx.TabBarItemData(id="Manual Input", title="✅ Manual Input", description="Pick your own Symbol or Ticker")])
 # dashboard title
-st.title("Sector Selection")
-
 option = st.selectbox(
-    'Which sector would you like to focus on?',
-    ('Energy', 'Tech', 'Financial'))
+            'Which sector would you like to focus on?',
+            ('Energy', 'Tech', 'Financial'))
 
 st.write('You selected:', option)
+
+st.title("Sector Selection")
+if tab == "Sector Select":
+    with st.form('Sector Select'):
+        submit_button = st.form_submit_button("Submit!")
+        placeholder_for_selectbox = st.empty()
+        
+        
+        with placeholder_for_selectbox:       
+            if option == 'Energy':
+                energy_option = st.selectbox('Please choose one of the sample stocks ',
+                                                ('XOM','CVX','ENB'))
+            elif option == 'Tech':
+                tech_option = st.selectbox('Please choose one of the sample stocks ',
+                                                ('APPL','NVDA','AVGO'))    
+            else:
+                fin_option = st.selectbox('Please choose one of the sample stocks ',
+                                                ('WFC','BAC','JPM'))
+            
+elif tab == "Manual Input":
+    with st.form("Manual Input"):
+        manual_submit_button = st.form_submit_button("Submit!")
+    symbol = st.text_input("Enter your other option...")
+    st.info(
+        f":white_check_mark: The written option is **{symbol}**")
+else:
+        st.error("`st.form_submit_button` has not been clicked yet")
