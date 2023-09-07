2e8517e1d60dd35c5da6e65a6019b41c82e5f5ac
Scott Speers <128268586+ScottyCodeman@users.noreply.github.com>
2023-09-07 18:07:41 -0400

    final update
    Final update before the presentation

--- hello.py
+++ hello.py
@@ -10,5 +10,6 @@ st.markdown(
 
     ### This is a link to our GitHub Repo[GitHub](https://github.com/chyj0805/Project_2)
 
+    ## This project is in no way meant to be used as advice and is not a recommendation to buy, sell or hold such investment or security or to make any other investment decisions. It is a proof of concept.
     """
 )
